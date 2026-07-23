3dce297591e50bff102c4d1581806786d93e0d3a
Zapihra <iida.vaaja@gmail.com>
2026-07-23 16:43:23 +0300

    working test_add_member

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,20 +6,5 @@ from modules.database import LibraryDatabase
 def connect_test_db():
     conn = LibraryDatabase("./database/test.db")
     conn.row_factory = sqlite3.Row
-    
-    #c = conn.cursor(":memory:")
-    
-    #c.execute(
-    #    '''CREATE TABLE users (
-    #        username TEXT,
-    #        password_hash TEXT
-    #    );''')
-    #conn.commit()
-
-    #c.execute("INSERT INTO users VALUES (?,?)", ("admin", "admin123"))
-    #conn.commit()
-
     yield conn
-
-    #conn.close()
 
--- tests/test_unit_tests.py
+++ tests/test_unit_tests.py
@@ -1,22 +1,28 @@
 import pytest
-from modules.database import LibraryDatabase
+from modules.database import LibraryDatabase as Libdb
 
 
 def test_authenticate_user(connect_test_db):
     
-    user = LibraryDatabase.authenticate_user(connect_test_db, "admin", "admin123")
+    user = Libdb.authenticate_user(connect_test_db, "admin", "admin123")
     assert user is not None
     assert user['username'] == "admin"
     assert user['password_hash'] == "pbkdf2_sha256$120000$f04b43f4cfd6f11eae7dff629a13ed7a$ae51eb988c0db7c7c9d77e7a37c56e366451e1a62ee921f1eb26ee91c2534c63"
-    
+
+def test_add_member(connect_test_db):
+    #in
+    new_member = {"member_code": "MB0002", "name": "abc", "email": "abc@abc", "phone":"123456", "address":"Lappeenranta"}
+    Libdb.add_member(connect_test_db, new_member)
+
+    user = Libdb.get_member_by_code(connect_test_db,"MB0002")
+
+    assert user['name'] == 'abc'
+    assert user['email'] == 'abc@abc'
+    assert user['phone'] == '123456'
+    assert user['address'] == 'Lappeenranta'
 
 @pytest.mark.skip()
 def test_build_heading():
     
     return
 
-@pytest.mark.skip()
-def test_add_user(connect_test_db):
-
-
-    return
